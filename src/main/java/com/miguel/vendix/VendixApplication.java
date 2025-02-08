db58ea71185a1361a87399bc5c4c68f23105e9b4
Miguel-Alonso638 <malonsor05@educarex.es>
2025-02-08 11:55:24 +0100

    Inicializancion del proyecto

---
+++ src/main/java/com/miguel/vendix/VendixApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.vendix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguel/vendix/VendixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.vendix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
