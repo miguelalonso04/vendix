5017397756e40fab1c5a4a033d49dc6618619291
Miguel Alonso <miguel.alonsor9@gmail.com>
2025-05-29 21:09:41 +0200

    cambios en rutas

--- backend/src/main/java/com/miguel/vendix/security/WebConfig.java
+++ backend/src/main/java/com/miguel/vendix/security/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:./uploads/");
+                .addResourceLocations("file:./uploads/", "file:/app/uploads");
     }
 }
 
