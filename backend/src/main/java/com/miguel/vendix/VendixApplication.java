baf3d19f5dc5c6bccd6adc4c1b6327b1d7e1f9cf
Miguel Alonso <miguel.alonsor9@gmail.com>
2025-05-29 15:57:31 +0200

    cambios sql

--- backend/src/main/java/com/miguel/vendix/VendixApplication.java
+++ backend/src/main/java/com/miguel/vendix/VendixApplication.java
@@ -1,7 +1,9 @@
 package com.miguel.vendix;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -11,5 +13,13 @@ public class VendixApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VendixApplication.class, args);
 	}
+
+    @Bean
+    ApplicationRunner delaySchedulerStart() {
+        return args -> {
+            // El scheduler empezará después de 10 segundos
+            Thread.sleep(10000);
+        };
+    }
 
 }
