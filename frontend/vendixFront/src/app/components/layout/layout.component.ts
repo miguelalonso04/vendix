ced77fa04fae18df7bebf303a56c11d7424253ac
Miguel Alonso <miguel.alonsor9@gmail.com>
2025-06-03 09:32:28 +0200

    cambios cs

--- frontend/vendixFront/src/app/components/layout/layout.component.ts
+++ frontend/vendixFront/src/app/components/layout/layout.component.ts
@@ -29,7 +29,7 @@ export class LayoutComponent {
     Libros: 'bi-book',
     Electrónica: 'bi-tablet',   
     Moda: 'bi-watch',               
-    Deportes: 'bi-dribble',          
+    Deportes: 'bi-trophy',          
     Automocion: 'bi-car-front-fill',
     Otros: 'bi-box'
   };
